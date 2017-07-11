fd4e8cf4aa35ea202c78afa69cdda038a4b6fe7f
Naseem Ahmad <naseem.ahmad@exilant.com>
2017-07-11 11:44:00 +0530

    Initial Commit

---
+++ EmployeeDetail_WatchOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmployeeDetail_WatchOS
+//
+//  Created by Naseem Ahmad on 11/07/17.
+//  Copyright © 2017 Naseem Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
